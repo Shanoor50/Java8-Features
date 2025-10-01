d470d38c7ac60a6849a6a77570896da83f4ee248
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-10-01 12:28:50 +0530

    Change3

--- AfterJava8_SolutionDM/src/com/nt/mobile/SIM.java
+++ AfterJava8_SolutionDM/src/com/nt/mobile/SIM.java
@@ -3,6 +3,7 @@ package com.nt.mobile;
 public interface SIM{
 	public void call();
 	public void sms();
+	//not implemented anyever
 	public default void videoCall() {
 		System.out.println("DF");
 	}
