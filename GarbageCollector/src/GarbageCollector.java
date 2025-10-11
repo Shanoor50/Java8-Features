579ce0081397209986df3c2526f87243216908b2
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-10-11 11:00:18 +0530

    Change3

--- GarbageCollector/src/GarbageCollector.java
+++ GarbageCollector/src/GarbageCollector.java
@@ -12,7 +12,7 @@
 }*/
 
 //we can explicitly call GC by using "System.gc()"
-public class GarbageCollector {
+/*public class GarbageCollector {
 	@Override
 	public void finalize() {
 		System.out.println("Memory cleared::"+Thread.currentThread().getName());
@@ -25,12 +25,56 @@ public class GarbageCollector {
 		ob2=ob3;//
 		new GarbageCollector();//Unreference object or Unused object
 		System.gc();// request to run garbage collector but not run bec If the JVM sees that there’s enough memory available, it will ignore the request.
-		System.out.println("program ends");
 		//why try block bec sometime we explicitly calling GC by using "System.gc()" but finalize method not called thats why we pausing main thread
 		try {
 			Thread.sleep(2000); //here we are pausing main thread bec whenever high priority thread like main work completed then JVM ignore low priority threads like GC
 		}catch(Exception e){
 			
 		}
+		System.out.println("program ends");
 	}
+}*/
+
+
+public class GarbageCollector {
+
+	static int a=0;
+    @Override
+    protected void finalize() {
+        System.out.println(++a +"=>Memory cleared by GC :: " + Thread.currentThread().getName());
+    }
+
+    static void m1() {
+        GarbageCollector ob4 = new GarbageCollector();
+        GarbageCollector ob5 = new GarbageCollector();
+        // ob4 and ob5 become unreferenced when m1() ends
+    }
+
+    static GarbageCollector m2() {
+        GarbageCollector ob6 = new GarbageCollector();
+        GarbageCollector ob7 = new GarbageCollector();
+        return ob7; // only ob6 becomes unreferenced
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        GarbageCollector ob1 = new GarbageCollector();
+        GarbageCollector ob2 = new GarbageCollector();
+        GarbageCollector ob3 = new GarbageCollector();
+
+        ob1 = null;              // eligible for GC
+        ob2 = ob3;               // old object referenced by ob2 is now eligible
+        new GarbageCollector();  // anonymous object, eligible immediately
+
+        m1();                    // creates 2 more (both eligible after m1 ends)
+        m2();                    // creates 2 (1 eligible, 1 returned & still referenced)
+
+        System.out.println("Requesting Garbage Collection...");
+        System.gc(); // request GC
+
+        // Give GC enough time to run and finalize
+        Thread.sleep(5000);
+
+        System.out.println("Program ends.");
+    }
 }
+
